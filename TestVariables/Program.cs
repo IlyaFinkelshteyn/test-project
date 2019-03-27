141db683ae9329bcbc3b72165141f64174c51c9a
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-03-27 11:55:24 -0700

    Test variables

---
+++ TestVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace TestVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+            Console.WriteLine("apiKey: " + configuration["apiKey"]);
+        }
+    }
+}
