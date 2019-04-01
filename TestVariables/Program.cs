cd1b893ba62dbbba385260cce7dda67c2cdb206c
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-04-01 09:04:24 -0700

    console color [skip ci]

--- TestVariables/Program.cs
+++ TestVariables/Program.cs
@@ -7,8 +7,13 @@ namespace TestVariables
     {
         static void Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
-            Console.WriteLine("apiKey: " + configuration["apiKey"]);
+            //var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+            //Console.WriteLine("apiKey: " + configuration["apiKey"]);
+            
+            // https://help.appveyor.com/discussions/questions/35756-consol-color-output?mail_type=queue
+            Console.ForegroundColor = ConsoleColor.Green; 
+            Console.WriteLine("Job is done");
+
         }
     }
 }
